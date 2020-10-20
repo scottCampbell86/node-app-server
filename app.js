e64b7534a4f3e9da25387ee982b16ba28e3feb3d
Scott Campbell <stc9486@gmail.com>
2020-10-20 18:46:47 -0400

    res

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.method, req.url, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<h1>Hello from my Node.js server!</h1>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
