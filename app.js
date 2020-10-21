09a6140e183d604e5dc2aedadc6286db63f5780d
Scott Campbell <stc9486@gmail.com>
2020-10-21 17:31:38 -0400

    modularize

--- app.js
+++ app.js
@@ -1,46 +1,8 @@
 const http = require('http');
-const fs = require('fs');
-const { isRegExp } = require('util');
+const routes = require('./routes');
 
   //create our server and define a function that fires everytime a request comes in;
-const server = http.createServer((req, res) => {
-
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<h1>Enter Message</h1>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<h1>Hello from my Node.js server!</h1>');
-  res.write('</html>');
-  res.end();
-  
-});
+const server = http.createServer(routes);
 
   //initialze our server to 'listen' for events;
 server.listen(3000);
